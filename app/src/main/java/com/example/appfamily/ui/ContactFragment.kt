07a6b97a2386601427e123fd6800d0690520775b
Miguel <miguel_ahumada_c@hotmail.com>
2021-02-16 20:36:23 -0300

    se completa el proceso de enviar un mensaje por whatsapp

--- app/src/main/java/com/example/appfamily/ui/ContactFragment.kt
+++ app/src/main/java/com/example/appfamily/ui/ContactFragment.kt
@@ -13,6 +13,7 @@ import com.example.appfamily.databinding.FragmentContactBinding
 
 class ContactFragment : Fragment() {
     private lateinit var binding: FragmentContactBinding
+    var option: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,23 +36,64 @@ class ContactFragment : Fragment() {
         binding.btnEnviar.isInvisible = true
         binding.btnSalir.isInvisible = true
 
+        binding.ivWsp.setOnClickListener {
+            binding.tvMail.isInvisible = true
+            binding.etMail.isInvisible = true
+            binding.etSubject.isInvisible = true
+            binding.tvSubject.isInvisible = true
+            binding.etTexto.isInvisible = false
+            binding.etTexto.text.clear()
+            binding.tvMessage.isInvisible = false
+            binding.btnEnviar.isInvisible = false
+            binding.btnSalir.isInvisible = false
+            option = 1
+        }
+
         binding.ivGmail.setOnClickListener {
             binding.tvMail.isInvisible = false
             binding.etMail.isInvisible = false
+            binding.etMail.text.clear()
             binding.etSubject.isInvisible = false
+            binding.etSubject.text.clear()
             binding.tvSubject.isInvisible = false
             binding.etTexto.isInvisible = false
+            binding.etTexto.text.clear()
             binding.tvMessage.isInvisible = false
             binding.btnSalir.isInvisible = false
             binding.btnEnviar.isInvisible = false
+            option = 0
         }
 
         binding.btnEnviar.setOnClickListener {
-            onIntent(
-                binding.etMail.text.toString(),
-                binding.etSubject.text.toString(),
-                binding.etTexto.text.toString()
-            )
+            if (option == 0){
+                onIntent(
+                    binding.etMail.text.toString(),
+                    binding.etSubject.text.toString(),
+                    binding.etTexto.text.toString()
+                )
+            } else if (option == 1) {
+                try {
+                    val intent = Intent(Intent.ACTION_SEND).apply {
+                        putExtra(Intent.EXTRA_TEXT, binding.etTexto.text.toString())
+                        putExtra("Miguel Ahumada", "${56951316411}@s.whatsapp.net")
+                        type = "text/plain"
+                        setPackage("com.whatsapp")
+                    }
+                    startActivity(intent)
+                }catch (e: Exception){
+                    e.printStackTrace()
+                    val appPackageName = "com.whatsapp"
+                    try {
+                        startActivity(Intent(Intent.ACTION_VIEW,
+                            Uri.parse("market://details?id=$appPackageName")))
+                    } catch (e :android.content.ActivityNotFoundException) {
+                        startActivity(Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://play.google.com/store/apps/details?id=" +
+                                    "$appPackageName")))
+                    }
+                }
+            }
+
         }
     }
 
